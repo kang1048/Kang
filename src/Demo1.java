d23dfa567c7d74520e9b00251d921631494468de
kang1048 <1847404558@qq.com>
2018-02-05 19:24:05 +0800

    测试HelloWorld

---
+++ src/Demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @Author:kangjunyu
+ * @Description:
+ * @Date Create in 18:29 2018/2/5
+ */
+public class Demo1 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
